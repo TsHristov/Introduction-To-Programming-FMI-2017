b465bb4ea3985d2de514045c153571a8fcf69cc2
TsHristov <tsvetanhristov@yahoo.com>
2017-11-07 22:38:38 +0200

    Add example solution to task 2

---
+++ Week4/Tasks/Solutions/task2_solution.cpp
@@ -0,0 +1,24 @@
+// УП-практикум, спец.Информатика, 6-та група, Седмица 3
+// Проверка за монотонно растяща редица:
+#include <iostream>
+using namespace std;
+
+int main(){
+  int size;
+  cout << "size=";
+  cin  >> size;
+  
+  int previous = 0;
+  int current  = 0;
+  bool flag    = true;
+  
+  for(int i=0; i < size; i++){
+    cin >> current;
+    if (previous > current) flag = false;
+    previous = current;
+  }
+
+  flag ? cout << "yes\n" : cout << "no\n";
+  
+  return 0;
+}
